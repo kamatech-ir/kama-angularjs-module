ec94f40bbb384c28c6d57cd0211b364bd49715d4
mohammadi <k.mohammadi.d.1366@gmail.com>
2022-08-31 10:33:43 +0430

    deploymentMode add

--- src/services/global.service.js
+++ src/services/global.service.js
@@ -1,67 +1,75 @@
 ﻿globalService.$inject = ['$window', '$rootScope'];
 export default function globalService($window, $rootScope) {
-  var service = {};
+    var service = {};
 
-  service.get = get;
-  service.set = set;
-  service.remove = remove;
+    service.get = get;
+    service.set = set;
+    service.remove = remove;
+    service.deploymentMode = deploymentMode;
+    service.prodoctionMode = prodoctionMode;
 
-  return service;
+    return service;
 
-  function get(key) {
-    try {
-      return changeStringToDate(
-        JSON.parse($window.localStorage.getItem(key)) || $rootScope[key]
-      );
+    function get(key) {
+        try {
+            return changeStringToDate(
+                JSON.parse($window.localStorage.getItem(key)) || $rootScope[key]
+            );
 
-      function changeStringToDate(item) {
-        if (Object.prototype.toString.call(item) === '[object Array]') {
-          for (var i = 0; i < item.length; i++) {
-            item[i] = changeStringToDate(item[i]);
-          }
-        } else if (Object.prototype.toString.call(item) === '[object Object]') {
-          for (var key in item) {
-            if (item.hasOwnProperty(key)) {
-              item[key] = changeStringToDate(item[key]);
+            function changeStringToDate(item) {
+                if (Object.prototype.toString.call(item) === '[object Array]') {
+                    for (var i = 0; i < item.length; i++) {
+                        item[i] = changeStringToDate(item[i]);
+                    }
+                } else if (Object.prototype.toString.call(item) === '[object Object]') {
+                    for (var key in item) {
+                        if (item.hasOwnProperty(key)) {
+                            item[key] = changeStringToDate(item[key]);
+                        }
+                    }
+                } else if (/-date-ms$/.test(item)) {
+                    item = new Date(parseInt(item));
+                }
+
+                return item;
             }
-          }
-        } else if (/-date-ms$/.test(item)) {
-          item = new Date(parseInt(item));
+        } catch (e) {
+            if (/-date-ms$/.test($window.localStorage.getItem(key))) {
+                return new Date(parseInt($window.localStorage.getItem(key)));
+            }
+            return $window.localStorage.getItem(key);
         }
-
-        return item;
-      }
-    } catch (e) {
-      if (/-date-ms$/.test($window.localStorage.getItem(key))) {
-        return new Date(parseInt($window.localStorage.getItem(key)));
-      }
-      return $window.localStorage.getItem(key);
     }
-  }
-  function set(key, value) {
-    $window.localStorage.setItem(key, JSON.stringify(changeDateToJson(value)));
+    function set(key, value) {
+        $window.localStorage.setItem(key, JSON.stringify(changeDateToJson(value)));
 
-    function changeDateToJson(item) {
-      if (Object.prototype.toString.call(item) === '[object Array]') {
-        for (var i = 0; i < item.length; i++) {
-          changeDateToJson(item[i]);
-        }
-      } else if (Object.prototype.toString.call(item) === '[object Object]') {
-        for (var key in item) {
-          if (item.hasOwnProperty(key)) {
-            changeDateToJson(item[key]);
-          }
-        }
-      } else if (Object.prototype.toString.call(item) === '[object Date]') {
-        item.toJSON = function () {
-          return this.getTime() + '-date-ms';
-        };
-      }
+        function changeDateToJson(item) {
+            if (Object.prototype.toString.call(item) === '[object Array]') {
+                for (var i = 0; i < item.length; i++) {
+                    changeDateToJson(item[i]);
+                }
+            } else if (Object.prototype.toString.call(item) === '[object Object]') {
+                for (var key in item) {
+                    if (item.hasOwnProperty(key)) {
+                        changeDateToJson(item[key]);
+                    }
+                }
+            } else if (Object.prototype.toString.call(item) === '[object Date]') {
+                item.toJSON = function () {
+                    return this.getTime() + '-date-ms';
+                };
+            }
 
-      return item;
+            return item;
+        }
     }
-  }
-  function remove(key) {
-    $window.localStorage.removeItem(key);
-  }
+    function remove(key) {
+        $window.localStorage.removeItem(key);
+    }
+    function deploymentMode() {
+        return (angular.element('input[name="deploymentMode"]').attr('value') === '1');
+    }
+    function prodoctionMode() {
+        return (angular.element('input[name="deploymentMode"]').attr('value') === '2');
+    }
 }
